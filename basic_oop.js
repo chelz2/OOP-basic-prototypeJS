d5bcaabddda7059c1e2ff68043d6ac318b0f8d0b
chelz2 <golirida@hotmail.com>
2024-07-25 04:42:43 +0530

    basic OOP prototype example in JS initial commit

---
+++ basic_oop.js
@@ -0,0 +1,38 @@
+function Player(name) {
+    this.name = name
+    this.level = 1
+    this.points = 0
+}
+
+Player.prototype.gainXp = function(xp) {
+    this.points += xp;
+
+    if (this.points >= 10) {
+        this.level += 1;
+        this.points -= 10; 
+    }
+
+    console.log(this.describe())
+}
+
+Player.prototype.describe = function() {
+    return `${this.name} is level ${this.level} with 
+    ${this.points} experience points `
+}
+
+const player1 = new Player('Bobby');
+const player2 = new Player('Alice');
+
+player1.gainXp(4);
+player2.gainXp(5);
+player2.gainXp(7);
+player1.gainXp(5);
+player1.gainXp(2);
+player2.gainXp(3);
+player1.gainXp(8);
+
+
+
+//console.log(player1.describe());
+//console.log(player2.describe());
+
